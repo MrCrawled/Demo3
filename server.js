7a025c017686271ef8d3d1de5cb5585db6bc6d53
Fernando Tepsich <fertepsi@gmail.com>
2021-06-12 16:36:29 -0300

    Last commit

--- server.js
+++ server.js
@@ -10,12 +10,4 @@ app.use(bodyParser.json());
 
 const port = 4000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-
-app.listen(port, ()=>{
-    console.log(`Server running at http://localhost:${port}`)
-    
-})
-
+console.log('Hello');
