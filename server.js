9385f7def7b9d0934ec8fa2b6dfde534df9d51cb
Fernando Tepsich <fertepsi@gmail.com>
2021-06-12 15:38:53 -0300

    Last commit

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
   })
 
 app.listen(port, ()=>{
-    console.log(`Server runningt http://localhost:${port}`)
+    console.log(`Server running at http://localhost:${port}`)
     
 })
 
