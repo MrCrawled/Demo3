f8467b603afc974734a36c2ebc277521a76073f5
Fernando Tepsich <fertepsi@gmail.com>
2021-06-12 15:21:34 -0300

    Last commit

--- server.js
+++ server.js
@@ -7,15 +7,4 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
-const port = 4000
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-
-app.listen(port, ()=>{
-    console.log(`Server runningt http://localhost:${port}`)
-    next();
-})
-
+console.log("No funca nada")
