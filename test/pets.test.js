776d538101b9340bf7551ad99ebbee1d486bb8e6
Fernando Tepsich <fertepsi@gmail.com>
2021-06-14 13:18:27 -0300

    Test

--- test/pets.test.js
+++ test/pets.test.js
@@ -1,11 +1,27 @@
 const db = require("../database/knex")
 
+
+
 beforeAll(async () => {
   // run the migrations and do any other setup here
   await db.migrate.latest()
 })
 
+afterAll(async () => {
+    await db.destroy()
+  })
+
 test("select pets", async () => {
   let pets = await db.from("pets").select("name")
   expect(pets.length).toBe(3)
 })
+
+test("insert pets", async () => {
+    await db("pets").insert([{
+        pet_type: 'Bird',
+        name: 'Tweety',
+        age: 2
+    }])
+  })
+  
+
